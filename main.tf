ecef274de827f13c285aaf77ae8292565e558465
EC2 Default User <ec2-user@ip-172-31-16-255.ap-northeast-1.compute.internal>
2024-06-09 13:24:11 +0000

    Add VPC and EC2 instance configuretions

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0b9a26d37416470d2"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleInstance"
-  }
-}
--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region     = "ap-northeast-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  profile = "terraform"
+  region  = "ap-northeast-1"
 }
