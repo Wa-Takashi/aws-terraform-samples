5a6f1ebf47747f6d459d0ef84dfb4845ca8503a4
EC2 Default User <ec2-user@ip-172-31-16-255.ap-northeast-1.compute.internal>
2024-06-09 14:57:50 +0000

    Add VPC, public and private subnets, internet gateway, NATgateway, and route table

--- vpc.tf
+++ vpc.tf
@@ -14,3 +14,52 @@ resource "aws_subnet" "private_subnet1" {
     Name = "private_subnet1"
   }
 }
+
+resource "aws_subnet" "public_subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_subnet1"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_internet_gateway"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet1.id
+
+  tags = {
+    Name = "main_nat_gateway"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "private_route_table"
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
